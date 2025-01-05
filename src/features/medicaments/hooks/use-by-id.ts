5f750b6f879388f2a47d5b792916e0ca39289c5c
JoseAndres <mostroso2003@gmail.com>
2025-01-04 22:15:46 -0400

    Añade hook useMedicamentById con react-query

--- src/features/medicaments/hooks/use-by-id.ts
+++ src/features/medicaments/hooks/use-by-id.ts
@@ -2,14 +2,13 @@ import { useQuery } from '@tanstack/react-query'
 
 export function useMedicamentById(id: string) {
   const { data, error, isLoading } = useQuery(
-    ['medicament', id],  // El queryKey que es único para cada medicament
-    () => fetchMedicamentById(id)  // queryFn: función que obtiene los datos del medicament
+    ['medicament', id],  // queryKey
+    fetchMedicamentById   // queryFn
   )
 
   return { data, error, isLoading }
 }
 
-// Esta es la función que hace la consulta para obtener el medicament por su ID
 async function fetchMedicamentById(id: string) {
   const response = await fetch(`/api/medicaments/${id}`)
   if (!response.ok) {
