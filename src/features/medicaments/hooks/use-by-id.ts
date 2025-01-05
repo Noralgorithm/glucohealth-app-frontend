03cff17162ed45705249bc6fd8b3b17800cf5ae1
JoseAndres <mostroso2003@gmail.com>
2025-01-04 22:22:30 -0400

    No se usa el useMedicamentById

--- src/features/medicaments/hooks/use-by-id.ts
+++ src/features/medicaments/hooks/use-by-id.ts
@@ -1,18 +1,5 @@
-import { useQuery } from '@tanstack/react-query'
+/*import { useQuery } from '@tanstack/react-query'
 
-export function useMedicamentById(id: string) {
-  const { data, error, isLoading } = useQuery(
-    ['medicament', id],  // queryKey
-    fetchMedicamentById   // queryFn
-  )
-
-  return { data, error, isLoading }
-}
-
-async function fetchMedicamentById(id: string) {
-  const response = await fetch(`/api/medicaments/${id}`)
-  if (!response.ok) {
-    throw new Error('Error fetching medicament data')
-  }
-  return response.json()
-}
+export function useMedicamentById() {
+  const {} = useQuery()
+}*/
